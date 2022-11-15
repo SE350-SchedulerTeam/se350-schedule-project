098dbedc283d940990cc18d55f014abdfbc1c58f
Dan <40920446+dan-k3@users.noreply.github.com>
2022-11-15 12:20:21 -0600

    Modify App.java

--- project/src/main/java/com/se350/scheduler/App.java
+++ project/src/main/java/com/se350/scheduler/App.java
@@ -1,6 +1,6 @@
 package com.se350.scheduler;
 
-import com.se350.scheduler.logic.DailyPlanner;
+// import com.se350.scheduler.logic.DailyPlanner;
 import com.se350.scheduler.logic.Event;
 import com.se350.scheduler.logic.Notification;
 import javafx.application.Application;
@@ -11,9 +11,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
+// JavaFX App
 public class App extends Application {
 
     private static Scene scene;
@@ -21,8 +19,13 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 806, 634);
+        // ResourceBundle bundle = ResourceBundle.getBundle("resources.UIResources");
+        // FXMLLoader loader = new FXMLLoader(getClass().getResource("project/src/main/resources/com/se350/scheduler/primary.fxml"), bundle);
+        // Parent root = loader.load();
+        stage.setTitle("Schedule Tracker");
         stage.setScene(scene);
         stage.show();
+        // new PrimaryController(c);
     }
 
     static void setRoot(String fxml) throws IOException {
