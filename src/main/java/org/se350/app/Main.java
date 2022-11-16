28abe411cff04bf751aa330ddade4d683f71a1f6
Dan <40920446+dan-k3@users.noreply.github.com>
2022-11-16 16:23:11 -0600

    Update Main.java

--- src/main/java/org/se350/app/Main.java
+++ src/main/java/org/se350/app/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 // JavaFX App
 public class Main extends Application {
@@ -12,10 +13,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/se350/gui/primary.fxml"));
             Parent root = loader.load();
             stage.setScene(new Scene(root));
             stage.setTitle("Schedule Tracker");
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UTILITY);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
