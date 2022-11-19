fddc1f70dc97a4107db68f97d199fecdfd563464
‘Elena Voinu <lenka36@gmail.com>
2022-11-19 15:25:07 -0600

    Implementing add events tests

--- src/test/java/org/se350/logic/DailyPlannerTest.java
+++ src/test/java/org/se350/logic/DailyPlannerTest.java
@@ -4,30 +4,58 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.*;
+import static org.se350.logic.EventType.SCHOOL;
+import static org.se350.logic.EventType.WORK;
 
 
 public class DailyPlannerTest {
 
     private DailyPlanner dailyPlanner;
     private List<Event> eventList;
+    private Event event;
     private Notification notification;
-    //  TimeSlot timeSlot = new TimeSlot();
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate localDate;
+    TimeSlot timeSlot;
     @Before
     public void setUp() throws Exception {
         dailyPlanner = new DailyPlanner();
         eventList = new ArrayList<>();
         notification = new Notification();
-
+        startTime = LocalTime.of(10, 30);
+        endTime = LocalTime.of(11, 0);
+        localDate = LocalDate.of(2022, 11, 25);
+        timeSlot = new TimeSlot(localDate, startTime, endTime);
+        event = new Event("Task","Attend Meeting", WORK, timeSlot);
     }
-    //   public Event(String name, String desc, EventType type, TimeSlot timeslot)
-    // public TimeSlot (LocalDate date, LocalTime start_time, LocalTime end_time)
+
     @Test
     public void addEvent() {
-        //  eventList.add(new Event("Task","Attend Meeting", WORK, "" ));
+         eventList.add(event);
+         assertNotNull(eventList);
+         assertEquals(localDate, event.getTimeslot().getDate());
+         assertEquals(startTime, event.getTimeslot().getStart_time());
+         assertEquals(endTime, event.getTimeslot().getEnd_time());
+         assertEquals(WORK, eventList.get(0).getType());
+         assertEquals("Attend Meeting", event.getDesc());
+         assertEquals("Task", event.getName());
+         assertEquals(1, eventList.size());
 
+         // add 1 more event
+        eventList.add(new Event("Homework", "Finish Unit Tests for Final Project",
+                SCHOOL,
+                new TimeSlot(LocalDate.of(2022, 11, 25),
+                        LocalTime.of(13, 10),
+                        LocalTime.of(17, 00)
+                        )));
+        assertEquals(2, eventList.size());
     }
 
     @Test
@@ -36,6 +64,8 @@ public class DailyPlannerTest {
 
     @Test
     public void getEvents() {
+        eventList.add(event);
+        assertEquals(event, eventList.get(0));
     }
 
     @Test
