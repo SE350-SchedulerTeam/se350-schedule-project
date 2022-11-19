41c64202f0c510a65693dc687e662ba1a1d3037a
‘Elena Voinu <lenka36@gmail.com>
2022-11-19 14:53:35 -0600

    Implementing test methods for registering observers and notifying them

--- src/test/java/org/se350/logic/DailyPlannerTest.java
+++ src/test/java/org/se350/logic/DailyPlannerTest.java
@@ -1,25 +1,37 @@
 package org.se350.logic;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+
 
 public class DailyPlannerTest {
 
+    private DailyPlanner dailyPlanner;
+    private List<Event> eventList;
+    private Notification notification;
+    //  TimeSlot timeSlot = new TimeSlot();
     @Before
     public void setUp() throws Exception {
-    }
+        dailyPlanner = new DailyPlanner();
+        eventList = new ArrayList<>();
+        notification = new Notification();
 
-    @After
-    public void tearDown() throws Exception {
     }
-
+    //   public Event(String name, String desc, EventType type, TimeSlot timeslot)
+    // public TimeSlot (LocalDate date, LocalTime start_time, LocalTime end_time)
     @Test
     public void addEvent() {
+        //  eventList.add(new Event("Task","Attend Meeting", WORK, "" ));
+
     }
 
     @Test
-    public void remove() {
+    public void removeEvent() {
     }
 
     @Test
@@ -28,9 +40,27 @@ public class DailyPlannerTest {
 
     @Test
     public void registerObserver() {
+        String expected = "Observer Registered";
+        Notification actual = new Notification("Observer Registered");
+        dailyPlanner.registerObserver(notification);
+        assertNotNull(dailyPlanner);
+        assertEquals(expected, actual.getDescription());
+        notification.showNotification("Observer Registered");
     }
 
     @Test
     public void notifyObservers() {
+        String description = "Notifying Observer";
+        Notification actual = new Notification("Notifying Observer");
+        notification.showNotification("Notifying Observer");
+        dailyPlanner.notifyObservers(description);
+        assertEquals(description, actual.getDescription());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // System.out.println("Tearing down");
+        dailyPlanner = null;
+        assertNull(dailyPlanner);
     }
 }
