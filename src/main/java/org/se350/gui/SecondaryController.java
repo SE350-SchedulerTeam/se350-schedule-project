c2a4312bc14a505b4c503a8213fc83f986296bf1
Dan <40920446+dan-k3@users.noreply.github.com>
2022-11-21 16:41:57 -0600

    Update SecondaryController.java

--- src/main/java/org/se350/gui/SecondaryController.java
+++ src/main/java/org/se350/gui/SecondaryController.java
@@ -10,7 +10,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-//import org.se350.app.Main;
 
 public class SecondaryController {
 
